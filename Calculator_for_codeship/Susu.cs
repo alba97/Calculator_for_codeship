4063c26f24fa3225e98eeedd04d0dc6aaaffcb34
Alevtina <alevtina_rain@mail.ru>
2017-05-24 14:56:29 +0400

    fist
    то что надо я надеюсь. с тестом

---
+++ Calculator_for_codeship/Susu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_for_codeship
+{
+   public class Susu
+    {
+
+
+        public int Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+
+        public int Mult(int num1, int num2)
+        {
+            int result1 = num1 * num2;
+            return result1;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator_for_codeship;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void test_Add_4and9_result13()
+        {
+
+           Susu some =new Susu();
+           int result = some.Add(4, 9);
+           Assert.AreEqual(13,result);
+        }
+
+        [TestMethod]
+        public void test_Mult_4and2_result8()
+        {
+
+            Susu some = new Susu();
+            int result = some.Mult(4, 2);
+            Assert.AreEqual(8, result);
+        }
+    }
+}
